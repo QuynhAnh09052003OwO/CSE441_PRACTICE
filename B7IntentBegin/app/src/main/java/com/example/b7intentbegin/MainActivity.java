9d3c8735318784d89578c5fcf797654987c55f87
2151062706@e.tlu.edu.vn <2151062706@e.tlu.edu.vn>
2024-10-17 23:45:55 +0700

    done

--- B7IntentBegin/app/src/main/java/com/example/b7intentbegin/MainActivity.java
+++ B7IntentBegin/app/src/main/java/com/example/b7intentbegin/MainActivity.java
@@ -25,5 +25,37 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        Button btn_child = findViewById(R.id.btn_child);
+        Button btn_bundle = findViewById(R.id.btn_bundle);
+        Button btn_bundle_update = findViewById(R.id.btn_bundle_update);
+
+        btn_child.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { startActivity("Child"); } });
+
+        btn_bundle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { startActivity("BundleTest"); } });
+
+        btn_bundle_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { startActivity("BundleUpdate"); } });
+
+    }
+    public void startActivity(String activityName) {
+        try {
+            // Lấy lớp Activity mục tiêu dựa trên tên được cung cấp
+            Class targetActivity = Class.forName(getPackageName() + "." + activityName);
+            // Tạo một Intent mới với lớp Activity mục tiêu
+            Intent myIntent = new Intent(MainActivity.this, targetActivity);
+            // Khởi động hoạt động
+            startActivity(myIntent);
+        } catch (ClassNotFoundException e) {
+            // Xử lý trường hợp lớp Activity không được tìm thấy
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
