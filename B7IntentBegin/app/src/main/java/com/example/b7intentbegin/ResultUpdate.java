899b67c7dc312708587702d801b20caed0652732
2151062706@e.tlu.edu.vn <2151062706@e.tlu.edu.vn>
2024-10-17 22:31:37 +0700

    update intent begun project

---
+++ B7IntentBegin/app/src/main/java/com/example/b7intentbegin/ResultUpdate.java
@@ -0,0 +1,18 @@
+package com.example.b7intentbegin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ResultUpdate extends AppCompatActivity implements View.OnClickListener {
+
+
+}
