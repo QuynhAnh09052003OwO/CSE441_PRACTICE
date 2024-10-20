1a8c3ac385aea3433b2eee9939d2aeb68e3c0108
2151062706@e.tlu.edu.vn <2151062706@e.tlu.edu.vn>
2024-10-20 22:17:58 +0700

    upload mainAct

--- B19/app/src/main/java/com/example/b19/MainActivity.java
+++ B19/app/src/main/java/com/example/b19/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.b19;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    Button btnParser;
+    ArrayAdapter<String> adapter;
+    List<String> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        listView = findViewById(R.id.list_view);
+        btnParser = findViewById(R.id.btn_parser);
+        list = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+        btnParser.setOnClickListener(v -> {
+            list.clear();
+            parseXML();
+            adapter.notifyDataSetChanged();
         });
     }
 }
