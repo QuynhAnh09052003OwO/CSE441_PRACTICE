734c0770bf5f0f6be9d611379783be6db97ba985
2151062706@e.tlu.edu.vn <2151062706@e.tlu.edu.vn>
2024-10-20 22:18:30 +0700

    update main

--- B19/app/src/main/java/com/example/b19/MainActivity.java
+++ B19/app/src/main/java/com/example/b19/MainActivity.java
@@ -39,5 +39,55 @@ public class MainActivity extends AppCompatActivity {
             parseXML();
             adapter.notifyDataSetChanged();
         });
-    }
+        private void parseXML() {
+            try {
+                InputStream myinput = getAssets().open("employee.xml");
+                XmlPullParserFactory fc = XmlPullParserFactory.newInstance();
+                XmlPullParser parser = fc.newPullParser();
+                parser.setInput(myinput,null);
+                int eventType=-1;
+                String nodeName;
+                String datashow="";
+                while(eventType!=XmlPullParser.END_DOCUMENT)
+                {
+                    eventType=parser.next();
+                    switch(eventType)
+                    {
+                        case XmlPullParser.START_DOCUMENT:
+                            break;
+                        case XmlPullParser.START_TAG:
+                            nodeName=parser.getName();
+                            if(nodeName.equals("employee")){
+                                datashow+=parser.getAttributeValue(0)+"-";
+                                datashow+=parser.getAttributeValue(1)+"-";
+                            }
+                            else if(nodeName.equals("name")){
+                                parser.next();
+                                datashow+=parser.getText()+"-";
+                            }
+                            else if(nodeName.equals("phone")){
+                                datashow+=parser.nextText();
+                            }
+                            break;
+                        case XmlPullParser.END_TAG:
+                            nodeName=parser.getName();
+                            if(nodeName.equals("employee")){
+                                list.add(datashow);
+                                datashow="";
+                            }
+                            break;
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            }
+            catch (IOException e1)
+            {
+                e1.printStackTrace();
+            }
+            catch (XmlPullParserException e2)
+            {
+                e2.printStackTrace();
+            }
+
+        }
 }
