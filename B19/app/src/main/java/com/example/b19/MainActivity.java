59182439bbfafb0340ebd98262754a3477d35322
2151062706@e.tlu.edu.vn <2151062706@e.tlu.edu.vn>
2024-10-20 22:19:05 +0700

    fix main

--- B19/app/src/main/java/com/example/b19/MainActivity.java
+++ B19/app/src/main/java/com/example/b19/MainActivity.java
@@ -11,6 +11,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
