e2a41008449cd92b8ee15823072686ac5bd9a92a
2151062706@e.tlu.edu.vn <2151062706@e.tlu.edu.vn>
2024-10-20 22:12:36 +0700

    upload main activity

--- B16/app/src/main/java/com/example/b16/MainActivity.java
+++ B16/app/src/main/java/com/example/b16/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.b16;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSum, btnClear;
+    private EditText inputA, inputB;
+    private TextView txtHistory;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    private static final String PREFS_NAME = "calcHistory";
+    private static final String KEY_HISTORY = "history";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
+
+        setupViews();
+        // Initialize Shared Preferences
+        sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        // Load saved history
+        loadHistory();
+
+        btnSum.setOnClickListener(v -> {
+            String a = inputA.getText().toString();
+            String b = inputB.getText().toString();
+            if (a.isEmpty() || b.isEmpty()) {
+                Toast.makeText(this, "Please enter both numbers", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            try {
+                int numA = Integer.parseInt(a);
+                int numB = Integer.parseInt(b);
+                int sum = numA + numB;
+                String history = sharedPreferences.getString(KEY_HISTORY, "");
+                String newEntry = numA + " + " + numB + " = " + sum;
+                history = newEntry + "\n" + history;
+
+                // Save updated history
+                editor.putString(KEY_HISTORY, history);
+                editor.apply();
+
+         
+
 }
