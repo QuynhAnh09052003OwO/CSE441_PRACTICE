ea8120134f888166c7a6475ea07a647965b0e039
2151062706@e.tlu.edu.vn <2151062706@e.tlu.edu.vn>
2024-10-20 22:12:52 +0700

    update main

--- B16/app/src/main/java/com/example/b16/MainActivity.java
+++ B16/app/src/main/java/com/example/b16/MainActivity.java
@@ -63,6 +63,34 @@ public class MainActivity extends AppCompatActivity {
                 editor.putString(KEY_HISTORY, history);
                 editor.apply();
 
-         
+                // Update TextView
+                txtHistory.setText(history);
+            }catch(NumberFormatException ex) {
+                Toast.makeText(this, "Please enter valid numbers", Toast.LENGTH_SHORT).show();
+                ex.printStackTrace();
+            }
+        });
+
+        btnClear.setOnClickListener(v -> {
+            txtHistory.setText("");
+            editor.putString(KEY_HISTORY, "");
+            editor.apply();
+        });
+
+    }
+
+    private void setupViews() {
+        btnSum = findViewById(R.id.btnSum);
+        btnClear = findViewById(R.id.btnClear);
+        inputA = findViewById(R.id.etNumberA);
+        inputB = findViewById(R.id.etNumberB);
+        txtHistory = findViewById(R.id.tvHistory);
+    }
+
+    private void loadHistory() {
+        // Load history from Shared Preferences
+        String history = sharedPreferences.getString(KEY_HISTORY, "");
+        txtHistory.setText(history);
+    }
 
 }
