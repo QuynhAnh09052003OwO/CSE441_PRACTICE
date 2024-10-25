4cefe29dda8baec7c24d3f05e3e94fe249238be7
2151062706@e.tlu.edu.vn <2151062706@e.tlu.edu.vn>
2024-10-26 00:56:14 +0700

    update

---
+++ NavigationDrawer/app/src/main/java/vn/edu/tlu/navigationdrawer/MainActivity.java
@@ -0,0 +1,30 @@
+package vn.edu.tlu.navigationdrawer;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        DrawerLayout drawer = findViewById(R.id.main);
+        drawer.openDrawer(GravityCompat.START);
+
+    }
+}
